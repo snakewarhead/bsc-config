f14203b2e7e81fe85f3dcabedfd5e4a9c408bb0a
linsilence <linlinsilence@gmail.com>
2022-05-11 12:28:54 +0800

    update

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ DIR_DATA="/wallet/bsc/data"
 $DIR_DATA/stop.sh
 echo -e "Starting \n"
 
-$DIR_DATA/geth_linux --config $DIR_DATA/config.toml --datadir $DIR_DATA --diffsync --rpc.allow-unprotected-txs --txlookuplimit 0 >$DIR_DATA/stdout.txt 2>$DIR_DATA/stderr.txt &
+$DIR_DATA/geth_linux --config $DIR_DATA/config.toml --datadir $DIR_DATA --diffsync --cache 8000 --rpc.allow-unprotected-txs --txlookuplimit 0 >$DIR_DATA/stdout.txt 2>$DIR_DATA/stderr.txt &
 echo $! >$DIR_DATA/node.pid
 
